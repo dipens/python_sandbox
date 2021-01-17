ec983a324e1d9770586a2be02795e7106b87d87e
dipens <dipenmshah8@gmail.com>
2021-01-17 12:05:08 -0500

    loops & modules

--- python_sandbox_starter/loops.py
+++ python_sandbox_starter/loops.py
@@ -1,5 +1,30 @@
 # A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
 
+people = ['John', 'Paul', 'Sara', 'Susan']
+for person in people:
+    print(f'Current Person: {person}')
 
 
+for person in people:
+    if(person == 'Sara'):
+        break
+    print(f'Current Person: {person}')
+
+for person in people:
+    if(person == 'Sara'):
+        continue
+    print(f'Current Person: {person}')
+
+
+for i in range(len(people)):
+    print(people[i])
+
+for i in range(0, 11):
+    print(i)
+
+
+count = 0
+while count <= 50:
+    print(count)
+    count += 1
 # While loops execute a set of statements as long as a condition is true.
--- python_sandbox_starter/modules.py
+++ python_sandbox_starter/modules.py
@@ -1,1 +1,12 @@
 # A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
+import datetime
+from datetime import date
+import time
+from time import time
+
+
+today = date.today()
+print(today)
+
+timestamp = time()
+print(timestamp)
