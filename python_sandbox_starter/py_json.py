9c6efac3fa851afc3cea96bdd7f5f757c4dfed97
dipens <dipenmshah8@gmail.com>
2021-01-17 12:33:58 -0500

    final commit

--- python_sandbox_starter/py_json.py
+++ python_sandbox_starter/py_json.py
@@ -1,1 +1,19 @@
 # JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary
+import json
+import urllib.request
+
+with urllib.request.urlopen("https://api.github.com/users") as url:
+    data = json.loads(url.read().decode())
+    for user in data:
+        print(user['login'])
+userJSON = '{"first_name": "John", "last_name": "Doe", "age":30}'
+
+user = json.loads(userJSON)
+
+
+print(user['first_name'])
+
+
+car = {'make': 'Ford', 'model': 'Mustang', 'year': '1970'}
+carJSON = json.dumps(car)
+print(carJSON)
