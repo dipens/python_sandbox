26a7577fb20b95747d09798d018106166dcdb3bd
dipens <dipenmshah8@gmail.com>
2021-01-17 12:23:59 -0500

    files basics

--- python_sandbox_starter/files.py
+++ python_sandbox_starter/files.py
@@ -1,1 +1,17 @@
 # Python has functions for creating, reading, updating, and deleting files.
+myFile = open('myFile.txt', 'a')
+
+
+print('Name: ', myFile.name)
+print('Is closed: ', myFile.closed)
+print('Mode: ', myFile.mode)
+
+
+myFile.write('I like Python\n')
+myFile.write(' and JS\n')
+myFile.close
+
+
+myFile = open('myFile.txt', 'r+')
+text = myFile.read(100)
+print(text)
